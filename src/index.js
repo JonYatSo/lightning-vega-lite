76f2baa21dc10517d4bbf4861ee6972d8b0004a7
Matthew Conlen <mc@mathisonian.com>
2015-09-24 19:32:58 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var LightningVega = require('lightning-vega');
+var vega = require('./deps/vega');
+var vl = require('vega-lite');
+
+/*
+ * Extend the base visualization object
+ */
+var Visualization = LightningVega.extend({
+
+    init: function() {
+        this.render();
+    },
+
+    render: function() {
+        var self = this;
+        vega.parse.spec(this.data, function(chart) {
+            var vis = chart({el: self.el, renderer: 'svg'});
+            vis.update();
+        });
+    },
+
+    formatData: function(data) {
+        var d = vl.compile(data.spec);
+        return d;
+    },
+
+    updateData: function(formattedData) {
+        this.data = formattedData;
+        this.render();
+    },
+
+    appendData: function(formattedData) {    
+    }
+});
+
+
+module.exports = Visualization;
